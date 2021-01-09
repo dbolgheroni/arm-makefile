c016da254c4a7e8849eca8743b33f9d6b02a89b4
Daniel Bolgheroni <dbolgheroni@protonmail.com>
2021-01-09 01:23:14 -0300

    Protect macro expansion

--- drv/gpio.h
+++ drv/gpio.h
@@ -33,19 +33,19 @@
 #define GPIO_CRL_MODEx_0(x)             (0x1U << GPIO_CRL_MODEx_Pos(x))
 #define GPIO_CRL_MODEx_1(x)             (0x2U << GPIO_CRL_MODEx_Pos(x))
 
-#define GPIO_CRH_MODEx_Pos(x)           (4U * (x - 8))
+#define GPIO_CRH_MODEx_Pos(x)           (4U * ((x) - 8))
 #define GPIO_CRH_MODEx_Msk(x)           (0x3U << GPIO_CRH_MODEx_Pos(x))
-#define GPIO_CRH_MODEx(x)               GPIO_CRL_MODEx_Msk(x)
+#define GPIO_CRH_MODEx(x)               GPIO_CRH_MODEx_Msk(x)
 #define GPIO_CRH_MODEx_0(x)             (0x1U << GPIO_CRH_MODEx_Pos(x))
 #define GPIO_CRH_MODEx_1(x)             (0x2U << GPIO_CRH_MODEx_Pos(x))
 
-#define GPIO_CRL_CNFx_Pos(x)            (4U * (x))
+#define GPIO_CRL_CNFx_Pos(x)            ((4U * (x)) + 2)
 #define GPIO_CRL_CNFx_Msk(x)            (0x3U << GPIO_CRL_CNFx_Pos(x))
 #define GPIO_CRL_CNFx(x)                GPIO_CRL_CNFx_Msk(x)
 #define GPIO_CRL_CNFx_0(x)              (0x1U << GPIO_CRL_CNFx_Pos(x))
 #define GPIO_CRL_CNFx_1(x)              (0x2U << GPIO_CRL_CNFx_Pos(x))
 
-#define GPIO_CRH_CNFx_Pos(x)            (4U * (x - 8))
+#define GPIO_CRH_CNFx_Pos(x)            ((4U * ((x) - 8)) + 2)
 #define GPIO_CRH_CNFx_Msk(x)            (0x3U << GPIO_CRH_CNFx_Pos(x))
 #define GPIO_CRH_CNFx(x)                GPIO_CRH_CNFx_Msk(x)
 #define GPIO_CRH_CNFx_0(x)              (0x1U << GPIO_CRH_CNFx_Pos(x))
@@ -55,11 +55,11 @@
 #define GPIO_BSRR_BSx_Msk(x)            (0x1U << GPIO_BSRR_BSx_Pos(x))
 #define GPIO_BSRR_BSx(x)                GPIO_BSRR_BSx_Msk(x)
 
-#define GPIO_BSRR_BRx_Pos(x)            (16U + x)
+#define GPIO_BSRR_BRx_Pos(x)            (16U + (x))
 #define GPIO_BSRR_BRx_Msk(x)            (0x1U << GPIO_BSRR_BRx_Pos(x))
 #define GPIO_BSRR_BRx(x)                GPIO_BSRR_BRx_Msk(x)
 
-#define GPIO_ODR_ODRx_Pos(x)            (0U + x)
+#define GPIO_ODR_ODRx_Pos(x)            (0U + (x))
 #define GPIO_ODR_ODRx_Msk(x)            (0x1U << GPIO_ODR_ODRx_Pos(x))
 #define GPIO_ODR_ODRx(x)                GPIO_ODR_ODRx_Msk(x)
 
